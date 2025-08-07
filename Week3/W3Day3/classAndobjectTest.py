f0d54a0bf0ac85453bd4511ae878f4f9f7e56ac3
jayDevsTree <jay.p@devstree.in>
2025-08-07 13:02:35 +0530

    making calculator and train class that show user fair(random generated) from,to,status

--- Week3/W3Day3/classAndobjectTest.py
+++ Week3/W3Day3/classAndobjectTest.py
@@ -20,3 +20,32 @@ o1.Training()
 
 
     
+class Programmer:
+    def __init__ (self,name,id,role,company):
+        self.name = name
+        self.id = id
+        self.role = role
+        self.company = company
+    def info(self):
+        print(f'{self.name} in {self.company} and role {self.role} have id {self.id}')
+        
+        
+e1 = Programmer('jay',57,'python','DevsTree')
+e1.info()
+
+class Programmer2:
+    def __init__ (self):
+        self.name = input("Enter Name: ")
+        self.id = input("Enter Id: ")
+        self.role = input("Enter Role: ")
+        self.company = input("Enter Company: ")
+    def info(self):
+        print(f'{self.name} in {self.company} and role {self.role} have id {self.id}')
+        
+        
+e1 = Programmer2()
+e1.info()
+
+
+       
+        
