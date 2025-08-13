f7053dd05c4f1937794876c4add9412354ea3476
jayDevsTree <jay.p@devstree.in>
2025-08-13 10:01:39 +0530

    Fixing json opeartion function and making it reusalble methods

--- Week4/W4Day4/jsonCRUD.py
+++ Week4/W4Day4/jsonCRUD.py
@@ -1,23 +1,30 @@
 import json
 
-file ='sample2.json'
+file_path ='sample2.json'
 def readJson():
     print("Read Operation:")
-    with open(file,'r') as file:
+    with open(file_path,'r') as file:
         data = json.load(file)
         
     print(data)
 
 readJson()
 
-def writeJson(file,data):
-    with open(file, 'w') as file:
+def writeJson(file_path,data):
+    with open(file_path, 'w') as file:
        json.dump(data,file,indent=4)
     print("json write content Updated")
     
-def updateJson(file,data):
-    with open(file,'r') as f:
+def updateJson(file_path,data):
+    with open(file_path,'r') as f:
         json.load(f)
+        
+    data["hobby"]="chess"
+    data["skills"].append('C++')
+    
+    with open(file_path,'w') as f:
+        json.dump(data,f,indent=4)
+    print("json updated")
     
    
         
